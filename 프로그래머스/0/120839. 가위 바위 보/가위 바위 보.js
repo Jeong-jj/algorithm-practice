6247f91471136e3d8c915701a24f5765243585bd
JeongJun <rgfdds98@gmail.com>
2025-01-29 23:57:54 +0900

    [level 0] Title: 가위 바위 보, Time: 0.11 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.js
@@ -0,0 +1,9 @@
+function solution(rsp) {
+    const rspArr = [...rsp];
+    const resultArr = rspArr.map((arg) => {
+        return arg === '2' ? '0' :
+                    arg === '0' ? '5' : '2';
+    })
+    
+    return resultArr.join().replaceAll(',','')
+}
