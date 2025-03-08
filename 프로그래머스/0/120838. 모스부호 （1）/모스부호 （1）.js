4d6b0ff15f993fe538c12c204d681e16272b1326
JeongJun <rgfdds98@gmail.com>
2025-03-09 01:03:54 +0900

    [level 0] Title: 모스부호 (1), Time: 0.20 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120838. 모스부호 （1）/모스부호 （1）.js
@@ -0,0 +1,17 @@
+function solution(letter) {
+    const morse = { 
+        '.-':'a','-...':'b','-.-.':'c','-..':'d','.':'e','..-.':'f',
+        '--.':'g','....':'h','..':'i','.---':'j','-.-':'k','.-..':'l',
+        '--':'m','-.':'n','---':'o','.--.':'p','--.-':'q','.-.':'r',
+        '...':'s','-':'t','..-':'u','...-':'v','.--':'w','-..-':'x',
+        '-.--':'y','--..':'z'
+    };
+    let result = '';
+    const letterArr = letter.split(' ');
+    
+    for (l of letterArr) {
+        result += morse[l]
+    }
+    
+    return result;
+}
