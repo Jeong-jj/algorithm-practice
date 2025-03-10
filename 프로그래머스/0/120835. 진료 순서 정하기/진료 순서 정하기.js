9551adb1bdf7ad7fff5b30afe518c32cd54b6147
JeongJun <rgfdds98@gmail.com>
2025-03-10 23:58:26 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.js
+++ 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.js
@@ -1,11 +1,6 @@
 function solution(emergency) {
     const sortedArr = [...emergency].sort((a,b)=>b-a)
-    const nestedArr = sortedArr.map((e,i)=>[e,i+1]);
 
-    const result = emergency.map((e)=>{
-        for (a of nestedArr) {
-            if (e===a[0]) return a[1];
-        }
-    })
+    const result = emergency.map((e)=>sortedArr.indexOf(e)+1)
     return result;
 }
